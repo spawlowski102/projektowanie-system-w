6637b17796c4a4a8385d05fa64a0817396877927
spawlowski102 <s.pawlowski6@student.uw.edu.pl>
2025-02-25 14:31:40 +0100

    zaj2zad1.R

--- zaj2zad1.R
+++ zaj2zad1.R
@@ -9,21 +9,21 @@ freq_terms(text)
 library(ggplot2)
 
   
-  ggplot(frequent_terms, aes(x = WORD, y = FREQ)) 
-       geom_bar(stat = "identity", fill = "skyblue") 
-     labs(x = "S³owo", y = "Czêstoœæ") 
-      theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
-      ggtitle("Wykres czêstoœci s³ów")
+  ggplot(frequent_terms, aes(x = WORD, y = FREQ)) +
+       geom_bar(stat = "identity", fill = "skyblue")+ 
+     labs(x = "S³owo", y = "Czêstoœæ") +
++      theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+ +     ggtitle("Wykres czêstoœci s³ów")
  ggplot(frequent_terms, aes(y = WORD, x = FREQ))
-    geom_bar(stat = "identity", fill = "skyblue") 
-    labs(x = "S³owo", y = "Czêstoœæ") 
-      theme(axis.text.x = element_text(angle = 0, hjust = 1)) 
-      ggtitle("Wykres czêstoœci s³ów")
+  +  geom_bar(stat = "identity", fill = "skyblue") 
+   + labs(x = "S³owo", y = "Czêstoœæ") 
+    +  theme(axis.text.x = element_text(angle = 0, hjust = 1)) 
+     + ggtitle("Wykres czêstoœci s³ów")
  ggplot(frequent_terms, aes(x = FREQ, y = reorder(WORD, FREQ))) 
-     geom_bar(stat = "identity", fill = "skyblue", color = "darkblue", alpha = 0.8) 
-     labs(x = "Czêstoœæ", y = "S³owo") 
-     ggtitle("Wykres czêstoœci s³ów")
-      theme_minimal() +
+     +geom_bar(stat = "identity", fill = "skyblue", color = "darkblue", alpha = 0.8) 
+     +labs(x = "Czêstoœæ", y = "S³owo") 
+     +ggtitle("Wykres czêstoœci s³ów")
+      +theme_minimal() +
       theme(axis.text.y = element_text(size = 10), # Dostosowanie rozmiaru czcionki etykiet na osi Y
                        plot.title = element_text(hjust = 0.5, size = 16, face = "bold"), # Wyœrodkowanie i stylizacja tytu³u wykresu
                        panel.grid.major.y = element_blank(), # Usuniêcie g³ównych linii siatki poziomej
